2400791d4ca7cc1b3ab13f2e881b8c64d3999a76
Roger Vieira <roger.vieira@agibank.com>
2018-10-16 18:46:38 -0300

    Adicionada a matriz de confusão

--- iris.R
+++ iris.R
@@ -1,23 +1,46 @@
 library(rpart)
 library(rpart.plot)
-library(party)
+library(caret)
 
-df <- read.csv(file = "/home/roger/Repositories/ClassificationModels/telco_churn.csv", sep = ",", header = TRUE, dec = ".")
+# lendo o arquivo que contem o dataset
+df <- read.csv(file = "C:/Users/roger.vieira/Repositories/ClassificationModels/telco_churn.csv", sep = ",", header = TRUE, dec = ".")
 
-df$PaymentMethod = as.factor(df$PaymentMethod)
-df$PaperlessBilling = as.factor(df$PaperlessBilling)
-df$InternetService = as.factor(df$InternetService)
+# convertendo a forma de pagamento em uma variável categórica
+df$PaymentMethod <- as.factor(df$PaymentMethod)
+df$gender <- as.factor(df$PaymentMethod)
 
+# separandos o dataset em treino e validação
+index <- createDataPartition(df$Churn, list=FALSE)
+training <- df[index, ]
+validation <- df[-index, ]
 
+# verificando o tamanho dos datasets
+nrow(training)
+nrow(validation)
 
-fitTree <- ctree(
-    df$Churn
-    ~ df$MonthlyCharges
-    + df$tenure,
-    data=df
+# aplicando o modelo de árvore de decisão
+#decisionTreeModel <- rpart(
+#    training$Churn
+#    ~ training$MonthlyCharges
+#    + training$TotalCharges
+#    + training$PaymentMethod
+#    + training$tenure,
+#    data=training,
+#    method = "class"    
+#)
+
+decisionTreeModel <- rpart(
+    training$Churn
+    ~ training$PaymentMethod,
+    data=training,
+    method = "class"    
 )
 
-#table(predict(fitTree), df$Churn)
+# rodando a predição ao dataset de validação
+pred <- predict(decisionTreeModel, validation, type="class")
 
+# exibindo a matriz de confusão
+confusionMatrix(validation$Churn, pred)
 
-plot(fitTree)
+# plotando a árvore de decisão
+rpart.plot(decisionTreeModel)
