4d0b59d46d6093ce39b9315d7bbc374942b6f619
Roger Vieira <roger.vieira@agibank.com>
2018-10-17 17:25:05 -0300

    changed project to Python again

--- iris.R
+++ iris.R
@@ -3,7 +3,7 @@ library(rpart.plot)
 library(caret)
 
 # lendo o arquivo que contem o dataset
-df <- read.csv(file = "/home/roger/Repositories/ClassificationModels//telco_churn.csv", sep = ",", header = TRUE, dec = ".")
+df <- read.csv(file = "C:/Users/roger.vieira/Repositories/ClassificationModels//telco_churn.csv", sep = ",", header = TRUE, dec = ".")
 
 # separandos o dataset em treino e validação
 index <- createDataPartition(df$Churn, list=FALSE)
@@ -38,7 +38,7 @@ decisionTreePrediction <- predict(decisionTreeModel, validation, type="class")
 logisticRegressionPrediction <- predict(logisticRegressionModel, validation)
 
 # exibindo a matriz de confusão
-confusionMatrix(validation$Churn, decisionTreePrediction)
+confusionMatrix(decisionTreePrediction, validation$Churn)
 
 confusionMatrix(validation$Churn, logisticRegressionPrediction)
 
